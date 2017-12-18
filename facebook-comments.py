1bf00ffce8d9fb351e02c172486de3ea4ac79ad4
Ismael-Safadi <31775833+Ismael-Safadi@users.noreply.github.com>
2017-12-18 23:51:33 +0200

    Update facebook-comments.py

--- facebook-comments.py
+++ facebook-comments.py
@@ -35,14 +35,6 @@ def onc():
 
 
     graph_api_version = 'v2.9'
-   
-    # LHL's Facebook user id
-    
-
-    # the id of LHL's response post at https://www.facebook.com/leehsienloong/posts/1505690826160285
-    
-
-    # the graph API endpoint for comments on LHL's post
     url = 'https://graph.facebook.com/{}/{}_{}/comments'.format(graph_api_version, user_id, post_id)
 
     comments = []
@@ -50,26 +42,17 @@ def onc():
     r = requests.get(url, params={'access_token': access_token})
     while True:
         data = r.json()
-
-        # catch errors returned by the Graph API
         if 'error' in data:
             raise Exception(data['error']['message'])
-
-        # append the text of each comment into the comments list
         for comment in data['data']:
-            # remove line breaks in each comment
             text = comment['message'].replace('\n', ' ')
             comments.append(text)
 
         print('got {} comments'.format(len(data['data'])))
-
-        # check if there are more comments
         if 'paging' in data and 'next' in data['paging']:
             r = requests.get(data['paging']['next'])
         else:
             break
-
-    # save the comments to a file
     with open('comments.txt', 'w') as f:
         for comment in comments:
             comment=comment.encode('utf-8')
